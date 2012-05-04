86ca228e790059f24a8fe2e82b056a8cc9250d76
Javier Ailbirt <jailbirt@gmail.com>
2012-05-04 03:00:08 -0300

    Agrego comentarios, headers

--- parser4Dani.pl
+++ parser4Dani.pl
@@ -1,4 +1,14 @@
 #!/usr/bin/perl
+#Autor. jailbirt.
+#Extrae fragmentos de texto para traducir y los deja en formato csv.
+#Funciona para: phtml, html y js.
+#el delimitador único es '|' (pipe) y deja un .csv por cada archivo de entrada en el directorio resultados.
+#la primer columna queda intacta sin escapeo, la segunda es utf8, la tercera es para traducir.
+#
+#ATENCION, solo en el caso de js, se emplea un segundo delimitador en la segunda columna, el mismo es el %
+#esto es porque al traducir, se toma todo lo de la tercer columna, se hace el correspondiente html o utf encode
+#y se reemplaza por la primer columna, en los casos de javascript cada % es un sub string a reemplazar, dentro de la primer columna.
+#
 use strict;
 use HTML::TreeBuilder 3;
 use HTML::Entities;
